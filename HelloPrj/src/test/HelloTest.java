c3585cffc358eff45e7137cfeed69dcb3092ea92
javaman <lepelaka@gmail.com>
2023-04-27 17:19:14 +0900

    sts 커밋

--- HelloPrj/src/test/HelloTest.java
+++ HelloPrj/src/test/HelloTest.java
@@ -2,6 +2,9 @@ package test;
 
 public class HelloTest {
 	public static void main(String[] args) {
+		System.out.println("hello world4");
+		System.out.println("hello world3");
+		System.out.println("hello world2");
 		System.out.println("hello world");
 	}
 }
