20440162d17f69044fa2050383140dafc9bf1cc4
javaman <lepelaka@gmail.com>
2023-04-27 17:40:35 +0900

    branch commit - 소스트리

--- HelloPrj/src/aa/Test.java
+++ HelloPrj/src/aa/Test.java
@@ -3,5 +3,6 @@ package aa;
 public class Test {
   public static void main(String[] args) {
     System.out.println("aa.Test.main()");
+    System.out.println("aa.Test.main() : 2");
   }
 }
--- HelloPrj/src/test/HelloTest.java
+++ HelloPrj/src/test/HelloTest.java
@@ -4,6 +4,8 @@ public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world");
-		System.out.println("hello world");
+		System.out.println("hello world 2");
+		System.out.println("hello world 3");
+		System.out.println("hello world 4");
 	}
 }
