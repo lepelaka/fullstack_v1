99a2e6cc4cf9727a25503907d2453d41e96721bf
javaman <lepelaka@gmail.com>
2023-04-27 17:40:09 +0900

    branch commit

--- HelloPrj/src/test/HelloTest.java
+++ HelloPrj/src/test/HelloTest.java
@@ -2,6 +2,8 @@ package test;
 
 public class HelloTest {
 	public static void main(String[] args) {
+		System.out.println("hello world6");
+		System.out.println("hello world5");
 		System.out.println("hello world4");
 		System.out.println("hello world3");
 		System.out.println("hello world2");
